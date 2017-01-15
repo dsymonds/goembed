99ca9731deac6c0e5317a0c5dd85245124b28f3d
Michael Stapelberg <stapelberg@google.com>
2017-01-15 14:39:00 -0800

    Implement embedding multiple files (#2)

--- goembed.go
+++ goembed.go
@@ -6,6 +6,7 @@ import (
 	"compress/gzip"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,34 +22,75 @@ var (
 func main() {
 	flag.Parse()
 
-	raw, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("Reading stdin: %v", err)
+	fmt.Printf("package %s\n\n", *packageFlag)
+
+	if *gzipFlag {
+		err := gzipPrologue.Execute(os.Stdout, map[string]interface{}{
+			"Args":    flag.Args(),
+			"VarName": *varFlag,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	fmt.Printf("package %s\n\n", *packageFlag)
+	if flag.NArg() > 0 {
+		fmt.Println("// Table of contents")
+		fmt.Printf("var %v = map[string][]byte{\n", *varFlag)
+		for i, filename := range flag.Args() {
+			fmt.Printf("\t%#v: %s_%d,\n", filename, *varFlag, i)
+		}
+		fmt.Println("}")
+
+		// Using a separate variable for each []byte, instead of
+		// combining them into a single map literal, enables a storage
+		// optimization: the compiler places the data directly in the
+		// program's noptrdata section instead of the heap.
+		for i, filename := range flag.Args() {
+			if err := oneVar(fmt.Sprintf("%s_%d", *varFlag, i), filename); err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		if err := oneVarReader(*varFlag, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func oneVar(varName, filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return oneVarReader(varName, f)
+}
+
+func oneVarReader(varName string, r io.Reader) error {
+	raw, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
 
 	// Generate []byte(<big string constant>) instead of []byte{<list of byte values>}.
 	// The latter causes a memory explosion in the compiler (60 MB of input chews over 9 GB RAM).
 	// Doing a string conversion avoids some of that, but incurs a slight startup cost.
 	if !*gzipFlag {
-		fmt.Printf(`var %s = []byte("`, *varFlag)
+		fmt.Printf(`var %s = []byte("`, varName)
 	} else {
 		var buf bytes.Buffer
 		gzw, _ := gzip.NewWriterLevel(&buf, gzip.BestCompression)
 		if _, err := gzw.Write(raw); err != nil {
-			log.Fatal(err)
+			return err
 		}
 		if err := gzw.Close(); err != nil {
-			log.Fatal(err)
+			return err
 		}
 		gz := buf.Bytes()
 
-		if err := gzipPrologue.Execute(os.Stdout, *varFlag); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("var %s []byte // set in init\n\n", *varFlag)
-		fmt.Printf(`var %s_gzip = []byte("`, *varFlag)
+		fmt.Printf("var %s []byte // set in init\n\n", varName)
+		fmt.Printf(`var %s_gzip = []byte("`, varName)
 		raw = gz
 	}
 
@@ -56,6 +98,7 @@ func main() {
 		fmt.Printf("\\x%02x", b)
 	}
 	fmt.Println(`")`)
+	return nil
 }
 
 var gzipPrologue = template.Must(template.New("").Parse(`
@@ -66,14 +109,34 @@ import (
 )
 
 func init() {
-	r, err := gzip.NewReader(bytes.NewReader({{.}}_gzip))
+	var (
+		r *gzip.Reader
+		err error
+	)
+
+{{ if gt (len .Args) 0 }}
+{{ range $idx, $var := .Args }}
+{{ $n := printf "%s_%d" $.VarName $idx }}
+	r, err = gzip.NewReader(bytes.NewReader({{ $n }}_gzip))
 	if err != nil {
 		panic(err)
 	}
-	defer r.Close()
-	{{.}}, err = ioutil.ReadAll(r)
+	{{ $n }}, err = ioutil.ReadAll(r)
+	r.Close()
 	if err != nil {
 		panic(err)
 	}
+{{ end }}
+{{ else }}
+	r, err = gzip.NewReader(bytes.NewReader({{ .VarName }}_gzip))
+	if err != nil {
+		panic(err)
+	}
+	{{ .VarName }}, err = ioutil.ReadAll(r)
+	r.Close()
+	if err != nil {
+		panic(err)
+	}
+{{ end }}
 }
 `))
